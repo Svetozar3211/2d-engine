9600ba63dc54b04c662b138990c94c58ced797cb
Svetozar <Svetozar@TOPINAMBUR>
2024-10-26 20:46:24 +0400

    Добавьте файлы проекта.

---
+++ 2d engine.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int plrX = 0;
+int plrY = 0;
+
+int wall1X = 2;
+int wall1Y = 2;
+
+string symbols[5] = {"  ", "░░", "▒▒", "▓▓", "██"};
+
+int main()
+{
+    
+}
